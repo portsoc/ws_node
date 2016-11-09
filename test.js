60acd6a1415bbff7d1ae4f0f64a3fd926da17cdd
Jacek Kopecký <KopeckyJ@uni.ds.port.ac.uk>
2016-11-09 15:56:34 +0000

    :art: code consistency

--- test.js
+++ test.js
@@ -14,10 +14,10 @@ test(
   "Create a file `" + pathUtil + "`",
   function () {
     try {
-        fs.accessSync(dir+pathUtil, fs.F_OK);
-        ok(true, pathUtil + " created");
+      fs.accessSync(dir+pathUtil, fs.F_OK);
+      ok(true, pathUtil + " created");
     } catch (e) {
-        ok(false, pathUtil + " is missing - please create it");
+      ok(false, pathUtil + " is missing - please create it");
     }
 });
 
@@ -51,48 +51,47 @@ test(
  */
 test(
   "Compare Arrays",
-    function(assert) {
+    function() {
       var util = require(dir+pathUtil);
-        assert.ok(
+        ok(
           typeof util.compare === "function",
           "Create a function called compare."
         );
 
-        assert.ok(
-            util.compare([1], [1]),
-            "two arrays with 1, should pass"
+        ok(
+          util.compare([1], [1]),
+          "two arrays with 1, should pass"
         );
 
-        assert.ok(
-            util.compare([1, 2, 3], [1, 2, 3]),
-            "two identical arrays, should pass"
+        ok(
+          util.compare([1, 2, 3], [1, 2, 3]),
+          "two identical arrays, should pass"
         );
 
-        assert.ok(
-            util.compare([4, 7, 11, 17], [4, 7, 11, 17]),
-            "two arrays, four idential elements each, should pass"
+        ok(
+          util.compare([4, 7, 11, 17], [4, 7, 11, 17]),
+          "two arrays, four idential elements each, should pass"
         );
 
-        assert.ok(
-            !util.compare([4, 7, 11, 17], [4, 7, 11]),
-            "different arrays that start the same, should not pass"
+        ok(
+          !util.compare([4, 7, 11, 17], [4, 7, 11]),
+          "different arrays that start the same, should not pass"
         );
 
-        assert.ok(
-            !util.compare([4, 7, 11, 17], [4, 7, 11, 3]),
-            "different arrays that start the same, should not pass"
+        ok(
+          !util.compare([4, 7, 11, 17], [4, 7, 11, 3]),
+          "different arrays that start the same, should not pass"
         );
 
-        assert.ok(
-            !util.compare([4, 7, 11, 17], [4, 17, 7, 11]),
-            "two arrays in different order, should not pass"
+        ok(
+          !util.compare([4, 7, 11, 17], [4, 17, 7, 11]),
+          "two arrays in different order, should not pass"
         );
 
-        assert.ok(
-            !util.compare([], [4, 17, 7, 11]),
-            "two arrays, one empty, should not pass"
+        ok(
+          !util.compare([], [4, 17, 7, 11]),
+          "two arrays, one empty, should not pass"
         );
-
     }
 );
 
@@ -102,17 +101,17 @@ test(
  * of numbers and returns the largest number.
  */
 test("Largest",
-    function(assert) {
+    function() {
       var util = require(dir+pathUtil);
-        assert.ok(
+        ok(
           typeof util.largest === "function",
           "Create a function called largest."
         );
-        assert.ok( util.largest([0,1,2]) == 2, "2 is the largest of 0,2 & 2" );
-        assert.ok( util.largest([1,1,3]) == 3, "2 is the largest of 1,1 & 3" );
-        assert.ok( util.largest([2,2,2]) == 2, "2 is the largest of 2, 2 & 2");
-        assert.ok( util.largest([1,2,3,4,5,6,7,8,3,-5]) == 8, "8 is th largest" );
-        assert.ok( util.largest([1]) == 1, "single element array works") ;
+        ok( util.largest([0,1,2]) == 2, "2 is the largest of 0,2 & 2" );
+        ok( util.largest([1,1,3]) == 3, "2 is the largest of 1,1 & 3" );
+        ok( util.largest([2,2,2]) == 2, "2 is the largest of 2, 2 & 2");
+        ok( util.largest([1,2,3,4,5,6,7,8,3,-5]) == 8, "8 is th largest" );
+        ok( util.largest([1]) == 1, "single element array works") ;
     }
 );
 
