1fbc47e99f32f8e2238830a3cc83643b8ee16766
Jacek Kopecky <jacek@jacek.cz>
2018-11-09 11:27:18 +0000

    let->const, also backticks

--- examples/http2.js
+++ examples/http2.js
@@ -1,13 +1,11 @@
-let http = require('http');
-let url = require('url');
-let server = http.createServer(
-    function (request, response) {
-        let parsedUrl = url.parse(request.url, true);
-        response.setHeader("Content-Type", "text/plain");
-        response.end(
-          'Hello ' + parsedUrl.query.fname +
-          ' ' + parsedUrl.query.lname + '!\n');
-    }
+const http = require('http');
+const url = require('url');
+const server = http.createServer(
+  function (request, response) {
+    let parsedUrl = url.parse(request.url, true);
+    response.setHeader("Content-Type", "text/plain");
+    response.end(`Hello ${parsedUrl.query.fname} ${parsedUrl.query.lname}!\n`);
+  }
 );
 server.listen(8080);
 // then go to http://localhost:8080/?lname=Solo&fname=Han
