1c7f689aa955eb946dc0098c2fa1bd082e02b066
Jacek Kopecky <jacek@jacek.cz>
2019-10-21 12:01:46 +0100

    update obj2.js to have 2 people

--- examples/obj2.js
+++ examples/obj2.js
@@ -1,15 +1,14 @@
-const person = {
-  name: "Rich",
-  age: 0
-};
+const person1 = { name: "Rich", age: 0 };
+const person2 = { name: "Matt", age: 18 };
 
 function becomeOlder(whom) {
   whom.age += 1;
 }
 
-function describePerson(whom) {
+function showAge(whom) {
   console.log(`${whom.name} is ${whom.age}`);
 }
 
-setInterval(becomeOlder, 1000, person);
-setInterval(describePerson, 1000, person);
+setInterval(becomeOlder, 2000, person1);
+setInterval(showAge, 1000, person1);
+setInterval(showAge, 500, person2);
